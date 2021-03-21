e4cb74d66a5c9e564bfa176e6f0b39c81fd4c636
Minoru Makino <makino111217@gmail.com>
2021-03-21 03:38:29 +0000

    [ADD]Userモデル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #nameのデータ操作を許可するアクションメソッドが指定されています。 sign_upアクションを呼ぶときにはnameのデータの操作が許可される。
+  end
+
+
 end
