bced55a18615f5b74ca80c3daf21a537f665012a
Minoru Makino <makino111217@gmail.com>
2021-03-21 05:01:07 +0000

    [ADD]画像投稿機能コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
   root to: 'homes#top'
   devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] #必要なアクションだけルートを設定するようにする
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
