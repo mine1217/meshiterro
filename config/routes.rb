9d18417f163a093fb2f0b061ff29cb96864fa123
Minoru Makino <makino111217@gmail.com>
2021-03-21 06:20:53 +0000

    [ADD]コメント機能の大枠

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] #必要なアクションだけルートを設定するようにする
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy
+    ]  
+  end#必要なアクションだけルートを設定するようにする
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
