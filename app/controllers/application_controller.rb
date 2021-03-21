38e43ff8d9d6f6b79674d2f629722a1a1c3a79d2
Minoru Makino <makino111217@gmail.com>
2021-03-21 06:02:10 +0000

    [ADD]ログイン後の遷移先を記事一覧ページへ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource) #ログイン後の遷移先
+    post_images_path
+  end
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #nameのデータ操作を許可するアクションメソッドが指定されています。 sign_upアクションを呼ぶときにはnameのデータの操作が許可される。
   end
